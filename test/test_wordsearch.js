ab84572842e19ae0273a69ccae7aa2149db9d249
Stefan <satalbot@protonmail.com>
2022-10-27 12:30:09 +0000

    Fix lint errors

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if FRANK is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['N', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['K', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
@@ -47,7 +47,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'G', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'O', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'VERTIGO')
+    ], 'VERTIGO');
 
     assert.isTrue(result);
   });
@@ -63,7 +63,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'MANGO')
+    ], 'MANGO');
 
     assert.isTrue(result);
   });
@@ -79,7 +79,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'PRESENT')
+    ], 'PRESENT');
 
     assert.isTrue(result);
   });
@@ -95,7 +95,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'H', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'G', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'GHOST')
+    ], 'GHOST');
 
     assert.isTrue(result);
   });
@@ -111,7 +111,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'H', 'W', 'P', 'P', 'A', 'I'],
       ['O', 'D', 'G', 'A', 'K', 'S', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'ASPRIN')
+    ], 'ASPRIN');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,66 +1,66 @@
 const { transpose } = require("../matrix-transposition");
 
 const wordSearch = (letters, word) => {
-    let horizontalJoin = letters.map(ls => ls.join('')) // Joins each "Row" of the letters matrix into a string, and maps those strings to a new array.
-    let horizontalReversed = reverseArray(horizontalJoin);
+  let horizontalJoin = letters.map(ls => ls.join('')); // Joins each "Row" of the letters matrix into a string, and maps those strings to a new array.
+  let horizontalReversed = reverseArray(horizontalJoin);
 
-    let verticalJoin = transpose(letters);
-    verticalJoin = verticalJoin.map(ls => ls.join('')) // Joins each "Row" of the transposed matrix into a string, and maps those strings to a new array.
-    let verticalReversed = reverseArray(verticalJoin);
+  let verticalJoin = transpose(letters);
+  verticalJoin = verticalJoin.map(ls => ls.join('')); // Joins each "Row" of the transposed matrix into a string, and maps those strings to a new array.
+  let verticalReversed = reverseArray(verticalJoin);
 
-    let diagonalJoin = transposeDiagonal(letters);
-    diagonalJoin = diagonalJoin.map(ls => ls.join('')); // Joins each "Row" of the transposed matrix into a string, and maps those strings to a new array.
-    let diagonalReverse = reverseArray(diagonalJoin);
+  let diagonalJoin = transposeDiagonal(letters);
+  diagonalJoin = diagonalJoin.map(ls => ls.join('')); // Joins each "Row" of the transposed matrix into a string, and maps those strings to a new array.
+  let diagonalReverse = reverseArray(diagonalJoin);
 
-    // Concat all arrays into one
-    let masterArray = horizontalJoin.concat(verticalJoin)
-        .concat(diagonalJoin)
-        .concat(horizontalReversed)
-        .concat(verticalReversed)
-        .concat(diagonalReverse);
+  // Concat all arrays into one
+  let masterArray = horizontalJoin.concat(verticalJoin)
+    .concat(diagonalJoin)
+    .concat(horizontalReversed)
+    .concat(verticalReversed)
+    .concat(diagonalReverse);
     
-    // Check if the master array contains the word
-    for (l of masterArray) {
-        if (l.includes(word)) {
-            return true;
-        }
+  // Check if the master array contains the word
+  for (const l of masterArray) {
+    if (l.includes(word)) {
+      return true;
     }
+  }
     
-    return false;
-}
+  return false;
+};
 
-const reverseArray = function (array) {
-    let reversedArray = array.map((element) => element.split("").reverse().join(""));
-    return reversedArray;
+const reverseArray = function(array) {
+  let reversedArray = array.map((element) => element.split("").reverse().join(""));
+  return reversedArray;
 };
 
 
 // Returns a matrix of values grabbed diagonally from the given matrix
 const transposeDiagonal = function(letters) {
-    let newMatrix = [];
-    // Make diagonal matrix, starting with the colums.
-    for(let c = 0; c < letters[0].length; c++) {
-        let tempArray = [];
-        for(let x = 0; x < letters[0].length - c; x++) {
-            let y = c + x
-            tempArray.push(letters[x][y]);
-        }
-        
-        newMatrix.push(tempArray)
+  let newMatrix = [];
+  // Make diagonal matrix, starting with the colums.
+  for (let c = 0; c < letters[0].length; c++) {
+    let tempArray = [];
+    for (let x = 0; x < letters[0].length - c; x++) {
+      let y = c + x;
+      tempArray.push(letters[x][y]);
     }
-
-    // Make diagonal matrix, ROW.
-    for(let r = 1; r < letters.length; r++) {
-        let tempArray = [];
-        for(let y = 0; y < letters.length - r; y++) {
-            let x = r + y
-            tempArray.push(letters[x][y]);
-        }
         
-        newMatrix.push(tempArray)
-    }
+    newMatrix.push(tempArray);
+  }
 
-    return newMatrix;
+  // Make diagonal matrix, ROW.
+  for (let r = 1; r < letters.length; r++) {
+    let tempArray = [];
+    for (let y = 0; y < letters.length - r; y++) {
+      let x = r + y;
+      tempArray.push(letters[x][y]);
+    }
+        
+    newMatrix.push(tempArray);
+  }
+
+  return newMatrix;
 };
 
-module.exports = wordSearch
+module.exports = wordSearch;
